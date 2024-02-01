8e7f5d8f53753c5daa92bb9331979155c44bb5c8
LPC <laurencecook80@hotmail.co.uk>
2024-02-01 17:46:36 +0000

    added media queries to Work, and fixed the ticker

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -10,7 +10,7 @@ export default function Work({ mode, setMode }) {
 
     useEffect(() => {
         setMode("dark");
-    },[])
+    },[mode, setMode])
     
     return (
         <section className={styles.container}>
@@ -18,80 +18,92 @@ export default function Work({ mode, setMode }) {
                 <Link to={ROUTES.ALLpaQ()}>
                     <figure className={styles.project}>
                         <img src={workImages[0].src} alt="click to go to ALLpaQ page" />
-                        <figcaption className={styles.heading}>ALLpaQ</figcaption>
-                        <figcaption className={styles.caption}>
-                            <hr/>
-                            <ul>
-                                <li>Digital design</li>
-                                <li>UX / UI</li>
-                            </ul>
-                        </figcaption>
+                        <div className={styles.projectInfo}>
+                            <figcaption className={styles.heading}>ALLpaQ</figcaption>
+                            <figcaption className={styles.caption}>
+                                <hr/>
+                                <ul>
+                                    <li>Digital design</li>
+                                    <li>UX / UI</li>
+                                </ul>
+                            </figcaption>
+                        </div>
                     </figure>
                 </Link>
                 <Link to={ROUTES.tsoHost()}>
                     <figure className={styles.project}>
                         <img src={workImages[1].src} alt="click to go to tsoHost page" />
-                        <figcaption className={styles.heading}>tsoHost</figcaption>
-                        <figcaption className={styles.caption}>
-                            <hr/>
-                            <ul>
-                                <li>Digital design</li>
-                                <li>Video</li>
-                            </ul>
-                        </figcaption>
+                        <div className={styles.projectInfo}>
+                            <figcaption className={styles.heading}>tsoHost</figcaption>
+                            <figcaption className={styles.caption}>
+                                <hr/>
+                                <ul>
+                                    <li>Digital design</li>
+                                    <li>Video</li>
+                                </ul>
+                            </figcaption>
+                        </div>
                     </figure>
                 </Link>
                 <Link to={ROUTES.CMA()}>
                     <figure className={styles.project}>
                         <img src={workImages[2].src} alt="click to go to CMA page" />
-                        <figcaption className={styles.heading}>CMAgency</figcaption>
-                        <figcaption className={styles.caption}>
-                            <hr/>
-                            <ul>
-                                <li>Digital design</li>
-                                <li>UX / UI</li>
-                                <li>3D</li>
-                            </ul>
-                        </figcaption>
+                        <div className={styles.projectInfo}>
+                            <figcaption className={styles.heading}>CMAgency</figcaption>
+                            <figcaption className={styles.caption}>
+                                <hr/>
+                                <ul>
+                                    <li>Digital design</li>
+                                    <li>UX / UI</li>
+                                    <li>3D</li>
+                                </ul>
+                            </figcaption>
+                        </div>
                     </figure>
                 </Link>
                 <Link to={ROUTES.fundomundo()}>
                     <figure className={styles.project}>
-                        <img src={workImages[3].src} alt="click to go to Fundomundo page" />
-                        <figcaption className={styles.heading}>Fundomundo</figcaption>
-                        <figcaption className={`${styles.caption} ${styles.darkText}`}>
-                            <hr/>
-                            <ul>
-                                <li>Branding</li>
-                                <li>UX / UI</li>
-                            </ul>
-                        </figcaption>
+                        <img src={workImages[3].src} style={{ objectPosition: "left" }} alt="click to go to Fundomundo page" />
+                        <div className={styles.projectInfo}>
+                            <div className={styles.heading}>Fundomundo</div>
+                            <div className={`${styles.caption} ${styles.darkText}`}>
+                                <hr/>
+                                <ul>
+                                    <li>Branding</li>
+                                    <li>UX / UI</li>
+                                </ul>
+                            </div>
+                        </div>
                     </figure>
                 </Link>
                 <Link to={ROUTES.TheWomensExchange()}>
                     <figure className={styles.project}>
                         <img src={workImages[4].src} alt="click to go to The Women's Exchange page" />
-                        <figcaption className={styles.heading}>The Women's Exchange</figcaption>
-                        <figcaption className={styles.caption}>
-                            <hr/>
-                            <ul>
-                                <li>Branding</li>
-                                <li>UX / UI</li>
-                            </ul>
-                        </figcaption>
+                        <div className={styles.projectInfo}>
+                            <figcaption className={styles.heading}>The Women's Exchange</figcaption>
+                            <figcaption className={styles.caption}>
+                                <hr/>
+                                <ul>
+                                    <li>Branding</li>
+                                    <li>UX / UI</li>
+                                </ul>
+                            </figcaption>
+                        </div>
                     </figure>
                 </Link>
                 <Link to={ROUTES.APE()}>
                     <figure className={styles.project}>
                         <img src={workImages[5].src} alt="click to go to ALLpaQ Plastic Engineering page" />
-                        <figcaption className={`${styles.heading} ${styles.darkText}`}>ALLpaQ Plastic Engineering</figcaption>
-                        <figcaption className={`${styles.caption} ${styles.darkText}`}>
-                            <hr/>
-                            <ul>
-                                <li>Branding</li>
-                                <li>UX / UI</li>
-                            </ul>
-                        </figcaption>
+                        <div className={styles.projectInfo}>
+                            <figcaption className={`${styles.heading} ${styles.darkText}`}>ALLpaQ Plastic Engineering</figcaption>
+                            <figcaption className={`${styles.caption} ${styles.darkText}`}>
+                                <hr/>
+                                <ul>
+                                    <li>Branding</li>
+                                    <li>UX / UI</li>
+                                </ul>
+                            </figcaption>
+                        </div>
                     </figure>
                 </Link>
             </section>
